42a204467ae1fc6516ec48f0053c589230df3a5b
FJ <qq1084472249@gmail.com>
2023-06-08 22:54:46 +0800

    add:单文件组件

---
+++ 18_单文件组件/vue_test/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <school></school>
+    <student></student>
+</div>
+</template>
+
+<script>
+    //引入组件
+    import Student from './components/Student.vue'
+    import School from './components/School.vue'
+    export default {
+        name:"App",
+        components:{
+            Student,
+            School
+        }
+    }
+</script>
+
+<style>
+    .demo{
+        border:1px solid pink;
+        padding:10px;
+        margin:10px;
+    }
+</style>
---
+++ 18_单文件组件/vue_test/src/components/School.vue
@@ -0,0 +1,36 @@
+<template>
+    <!-- 组件的结构 -->
+    <div class="demo">
+        <h2>学校名称;{{name}}}</h2>
+        <p>学校地址{{address}}}</p>
+        <button @click="showName">点我显示学校名</button>
+    </div>
+</template>
+
+<script>
+    //组件交互相关的代码（数据方法等）
+    //export 暴露组件  
+    export default {
+        name: "School",
+        data(){
+            return{
+                name:'清华大学',
+                address:'北京市海淀区'
+            }
+        },
+        methods:{
+            showName(){
+                alert(this.name)
+            }
+        }
+    }
+    
+</script>
+<style>
+/* 组件的样式 */
+.demo{
+    border:1px solid #ccc;
+    padding:10px;
+    margin:10px;
+}
+</style>
