fcc155eaa15123d8e73f7c8eabf29c8b87fa18d3
FJ <qq1084472249@gmail.com>
2023-06-10 15:58:43 +0800

    add:scoped

---
+++ 第二章 VUE高级/vue_test/src/components/School.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="demo">
+      <h2 >学校名称: {{name|mySlice}}</h2>
+      <h2>学校地址：{{address}}</h2>
+    </div>
+  </template>
+<script>
+//引入一个混合
+
+export default {
+    name: 'School',
+    data(){
+        return {
+            name: '北京1大学atguigu',
+            address: '北京市海淀区',
+        }
+    },
+}
+</script>
+
+<style scoped>
+.demo {
+    background-color: orange;
+  }
+</style>
