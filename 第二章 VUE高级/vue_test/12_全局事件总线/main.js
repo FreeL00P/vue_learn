7ef4b98394989e4672f486b003f04f99db89a20e
FJ <qq1084472249@gmail.com>
2023-06-11 17:01:20 +0800

    add:TodoList全局事件总线

--- 第二章 VUE高级/vue_test/12_全局事件总线/main.js
+++ 第二章 VUE高级/vue_test/12_全局事件总线/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './APP.vue'
 Vue.config.productionTip = false
-
+//定义全局组件
+// const Demo = Vue.extend({})
+// const d = new Demo()
+// Vue.prototype.x = d;
 new Vue({
     el: '#app',
     render: h => h(App),
--- 第二章 VUE高级/vue_test/src/APP.vue
+++ 第二章 VUE高级/vue_test/src/APP.vue
@@ -1,32 +1,130 @@
 <template>
-  <div class="app">
-    <h1> {{ msg }}</h1>
-    <School/>
-    <Student/>
+<div id="root">
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <MyHeader @addTodo="addTodo"/>
+      <MyList :todos="todos" />
+      <MyFooter :todos="todos" @checkAll="checkAll"
+      @clearAllFinished="clearAllFinished"
+      />
+    </div>
   </div>
+</div>
 </template>
 
 <script>
-import Student from './components/Student'
-import School from './components/School'
+    import MyHeader from './components/MyHeader.vue'
+    import MyFooter from './components/MyFooter.vue'
+    import MyList from './components/MyList.vue'
     export default {
         name: 'App',
         components: {
-            Student,
-            School
+          MyHeader,
+          MyFooter,
+          MyList
         },
-        data() {
-            return {
-                msg: '你好',
-                stuName:'',
-                schName:''
+        data(){
+          return {
+            todos:JSON.parse(localStorage.getItem('todos'))||[]
+        }
+        },
+        methods:{
+          addTodo(todoObj){
+            console.log("我是App组件,我接收到了",todoObj)
+            this.todos.unshift(todoObj)
+          },
+          //取消勾选一个todo
+          checkTodo(id){
+              this.todos.forEach((item)=>{
+                if(item.id===id){
+                  item.isFinished=!item.isFinished
+                  console.log(item)
+                }
+              })
+          },
+          //删除一个todo
+          deleteTodo(id){
+            console.log("删除",id)
+           this.todos=this.todos.filter(item=>item.id!==id)
+          },
+          //全选或者全不选
+          checkAll(checked){
+            checked?
+            this.todos.forEach(todo=>todo.isFinished=true):
+            this.todos.forEach(todo=>todo.isFinished=false)
+          },
+          //清除所有已完成的
+          clearAllFinished(){
+            this.todos=this.todos.filter(todo=>!todo.isFinished)
+          }
+        },
+        watch:{
+          todos:{
+            deep:true,
+            handler(value){
+              localStorage.setItem("todos",JSON.stringify(value))
             }
+          }
         },
+        mounted(){
+          this.$bus.$on("deleteTodo",this.deleteTodo),
+          this.$bus.$on("checkTodo",this.checkTodo)
+        },
+        beforeDestroy(){
+          this.$bus.$off("deleteTodo"),
+          this.$bus.$off("checkTodo")
+        }
     }
 </script>
 
 <style>
-.app{
-  background-color: #ccc;
+/*base*/
+body {
+  background: #fff;
 }
+
+.btn {
+  /* display: inline-block; */
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+
+
+
+
+
+
+
 </style>
