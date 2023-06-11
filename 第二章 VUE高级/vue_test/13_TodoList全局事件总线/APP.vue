2a5baa98726bd04cc8af7a58f64ed945bdb30ed0
FJ <qq1084472249@gmail.com>
2023-06-11 17:02:48 +0800

    add:TodoList全局事件总线

---
+++ 第二章 VUE高级/vue_test/13_TodoList全局事件总线/APP.vue
@@ -0,0 +1,130 @@
+<template>
+<div id="root">
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <MyHeader @addTodo="addTodo"/>
+      <MyList :todos="todos" />
+      <MyFooter :todos="todos" @checkAll="checkAll"
+      @clearAllFinished="clearAllFinished"
+      />
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+    import MyHeader from './components/MyHeader.vue'
+    import MyFooter from './components/MyFooter.vue'
+    import MyList from './components/MyList.vue'
+    export default {
+        name: 'App',
+        components: {
+          MyHeader,
+          MyFooter,
+          MyList
+        },
+        data(){
+          return {
+            todos:JSON.parse(localStorage.getItem('todos'))||[]
+        }
+        },
+        methods:{
+          addTodo(todoObj){
+            console.log("我是App组件,我接收到了",todoObj)
+            this.todos.unshift(todoObj)
+          },
+          //取消勾选一个todo
+          checkTodo(id){
+              this.todos.forEach((item)=>{
+                if(item.id===id){
+                  item.isFinished=!item.isFinished
+                  console.log(item)
+                }
+              })
+          },
+          //删除一个todo
+          deleteTodo(id){
+            console.log("删除",id)
+           this.todos=this.todos.filter(item=>item.id!==id)
+          },
+          //全选或者全不选
+          checkAll(checked){
+            checked?
+            this.todos.forEach(todo=>todo.isFinished=true):
+            this.todos.forEach(todo=>todo.isFinished=false)
+          },
+          //清除所有已完成的
+          clearAllFinished(){
+            this.todos=this.todos.filter(todo=>!todo.isFinished)
+          }
+        },
+        watch:{
+          todos:{
+            deep:true,
+            handler(value){
+              localStorage.setItem("todos",JSON.stringify(value))
+            }
+          }
+        },
+        mounted(){
+          this.$bus.$on("deleteTodo",this.deleteTodo),
+          this.$bus.$on("checkTodo",this.checkTodo)
+        },
+        beforeDestroy(){
+          this.$bus.$off("deleteTodo"),
+          this.$bus.$off("checkTodo")
+        }
+    }
+</script>
+
+<style>
+/*base*/
+body {
+  background: #fff;
+}
+
+.btn {
+  /* display: inline-block; */
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+
+
+
+
+
+
+
+</style>
---
+++ 第二章 VUE高级/vue_test/13_TodoList全局事件总线/components/MyItem.vue
@@ -0,0 +1,74 @@
+<template>
+        <li >
+          <label>
+            <input type="checkbox" :checked="todo.isFinished" @click="handleCheck(todo.id)" />
+            <span> {{todo.title}}</span>
+          </label>
+          <button class="btn btn-danger" @click="handleDelete(todo.id)">删除</button>
+        </li>
+</template>
+
+<script>
+export default {
+    name:"MyItem",
+    //声明接受父组件传递过来的数据
+    props: ['todo'],
+    methods:{
+        handleCheck(id){
+          //通知App组件，取消勾选一个todo
+          //this.$emit("checkTodo",id)
+          this.$bus.$emit("checkTodo",id)
+        } ,
+        //删除
+        handleDelete(id){
+          console.log('click id',id);
+          //通知App组件，删除一个todo
+          //this.$emit("deleteTodo",id)
+          this.$bus.$emit("deleteTodo",id)
+        } 
+      }
+    }
+</script>
+
+<style>
+/*item*/
+li {
+  list-style: none;
+  height: 36px;
+  line-height: 36px;
+  padding: 0 5px;
+  border-bottom: 1px solid #ddd;
+}
+
+li label {
+  float: left;
+  cursor: pointer;
+}
+
+li label li input {
+  vertical-align: middle;
+  margin-right: 6px;
+  position: relative;
+  top: -1px;
+}
+
+li button {
+  float: right;
+  display: none;
+  margin-top: 3px;
+}
+
+li:before {
+  content: initial;
+}
+
+li:last-child {
+  border-bottom: none;
+}
+li:hover {
+  background: #f5f5f5;
+}
+li:hover button {
+  display: block;
+}
+</style>
