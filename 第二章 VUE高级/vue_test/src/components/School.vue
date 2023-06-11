571d5f9e423e5856d5eff6770ebb307ee0206c6f
FJ <qq1084472249@gmail.com>
2023-06-11 16:35:00 +0800

    add:全局事件总线

---
+++ 第二章 VUE高级/vue_test/src/components/School.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="school">
+      <h2 >学校名称: {{name}}</h2>
+      <h2>学校地址：{{address}}</h2>
+    </div>
+  </template>
+<script>
+//引入一个混合
+
+export default {
+    name: 'School',
+    data(){
+        return {
+            name: '湖南工程学院',
+            address: '湖南省湘潭市',
+        }
+    },
+    mounted(){
+      console.log("School mounted",this)
+      //接收学生的名称
+      this.x.$on("getStudentName",(data)=>{
+        console.log("School接收到了学生的名称",data)
+      })   
+    },
+    beforeDestroy(){
+        console.log("School beforeDestroy")
+        this.$bus.$off("getStudentName")
+    }
+}
+</script>
+
+<style scoped>
+.school {
+    background-color: orange;
+    padding: 5px;
+  }
+</style>
---
+++ 第二章 VUE高级/vue_test/src/components/Student.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="student">
+        <h2 >学生姓名：{{name}}</h2>
+        <h2>学生性别：{{sex}}</h2>
+        <button @click="sendStudentName">把学生名称给School</button>
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+        name: 'Student',
+        data() {
+            return {
+                name: '张三',
+                sex: '男',
+            }
+        },
+        methods:{
+            sendStudentName(){
+                this.x.$emit("getStudentName",this.name)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.student{
+    background-color: skyblue;
+    padding: 5px;
+    margin-top: 30px;
+}
+</style>
