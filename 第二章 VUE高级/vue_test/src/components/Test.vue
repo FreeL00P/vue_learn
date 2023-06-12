a93ca3f0b1599472f90e349e2f51b2571e03922a
FJ <qq1084472249@gmail.com>
2023-06-12 19:12:06 +0800

    add:vue动画

---
+++ 第二章 VUE高级/vue_test/src/components/Test.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <button @click="isShow=!isShow">显示/隐藏</button>
+    <transition appear>
+        <h1 v-show="isShow" >你好啊</h1>
+    </transition>
+    
+  </div>
+</template>
+
+<script>
+    export default {
+        name:"Test",
+        data(){
+            return {
+                isShow:true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    h1{
+        background-color: skyblue;
+    }
+    .v-enter-active{
+        animation: freeloop 1s;
+    }
+    .v-leave-active{
+        animation: freeloop 1s reverse;
+    }
+    @keyframes freeloop {
+        from {
+            transform: translateX(-100%);
+        }
+        to {
+            transform: translateX(0px);
+        }
+    }
+</style>
