5a69902c0db7c4c347455561e58efd3c3b46a593
FJ <qq1084472249@gmail.com>
2023-06-16 18:36:49 +0800

    add:缓存路由组件

---
+++ 第二章 VUE高级/vue_test/38_src_keep-alive组件缓存/components/Banner.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="col-xs-offset-2 col-xs-8">
+    <div class="page-header">
+      <h2>Vue Router Demo</h2>
+      <button @click="back">后退</button>
+      <button @click="forward">前进</button>
+      <button @click="test">Go</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Banner",
+  methods: {
+    back() {
+      this.$router.back();
+    },
+    forward() {
+      this.$router.forward();
+    },
+    test() {
+      //go(n) n为正数前进n个页面，n为负数后退n个页面
+      this.$router.go(-1);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
