aaeba538002ac11e2c13bc9fe775ed6dff4713c5
FJ <qq1084472249@gmail.com>
2023-06-16 17:58:15 +0800

    add:replace属性

---
+++ 第二章 VUE高级/vue_test/36_replace属性/pages/Detail.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <!-- <li>消息编号:{{ this.$route.params.id }}</li>
+    <li>消息内容:{{ this.$route.params.title }}</li> -->
+    <li>消息编号:{{ id }}</li>
+    <li>消息内容:{{ title }}</li>
+    <!-- <li>{{ a }}</li>
+    <li>{{ b }}</li> -->
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Detail",
+  props: ["id", "title"],
+};
+</script>
+
+<style>
+</style>
